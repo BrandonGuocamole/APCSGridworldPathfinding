98794700e8baf7e71b3d09f42dd7b7dd89acdd11
Leonard <Leonard@192.168.0.83>
2018-04-26 01:06:36 -0600

    brandi do you think this will work

--- src/org/pumatech/teams/daddies/StarDaddy.java
+++ src/org/pumatech/teams/daddies/StarDaddy.java
@@ -8,13 +8,48 @@ import org.pumatech.ctf.AbstractPlayer;
 import info.gridworld.grid.Location;
 
 public class StarDaddy extends AbstractPlayer {
-	private ArrayList<Location> open;
-	private ArrayList<Location> closed;
+	private ArrayList<Daddy> open;
+	private ArrayList<Daddy> closed;
+	private Location OGFlag;
+	private Location goal;
 	
 	public StarDaddy(Location startLocation) {
 		super(startLocation);
-		open = new ArrayList<Location>();
-		closed = new ArrayList<Location>();
+		open = new ArrayList<Daddy>();
+		closed = new ArrayList<Daddy>();
+	}
+	
+	public ArrayList<Location> getAllAdjacent(Location loc) {
+		ArrayList<Location> locs = new ArrayList<Location>();
+		for (int i = 0; i < 360; i += 45) {
+			if (getGrid().isValid(loc.getAdjacentLocation(i))) {
+				locs.add(loc.getAdjacentLocation(i));
+			}
+		}
+		return locs;
+	}
+
+	public ArrayList<Location> getAllEmptyAdjacent(Location location) {
+		ArrayList<Location> locs = new ArrayList<Location>();
+		for (int i = 0; i < 360; i = i + 45) {
+			Location loc = location.getAdjacentLocation(i);
+			if (getGrid().isValid(loc)) {
+				if (getGrid().get(loc) == null) {
+					locs.add(loc);
+				}
+			}
+		}
+		return locs;
+	}
+
+	public ArrayList<Location> getAdjacent(Location loc) {
+		ArrayList<Location> locs = getAllAdjacent(loc);
+		for (int i = 0; i < locs.size(); i++) {
+			if (getGrid().get(loc) == null && loc != getLocation() && getScore(loc, OGFlag) > 4) {
+				locs.add(loc.getAdjacentLocation(i));
+			}
+		}
+		return locs;
 	}
 	
 	public boolean teamFlag() {
@@ -26,25 +61,64 @@ public class StarDaddy extends AbstractPlayer {
 		}
 		return false;
 	}
+	
+	public Daddy openLowest() {
+		Daddy lowest = open.get(0);
+		for(int i = 0; i < open.size(); i++) {
+			if(lowest.getRank() > open.get(i).getRank()) {
+				lowest = open.get(i);
+			}
+		}
+		return lowest;
+	}
 
 	public int getPythag(Location a, Location b) {
 		return (int) (Math.pow(
 				Math.pow(Math.abs(a.getCol() - b.getCol()), 2 + Math.pow(Math.abs(a.getRow() - b.getRow()), 2)), 0.5));
 	}
+	
+	public int getScore(Location a, Location b) {
+		return Math.abs(a.getCol() - b.getCol()) + Math.abs(a.getRow() - b.getRow());
+	}
+	
+	public int getCost(Location loc) {
+		// Weight based on surroundings
+		return 0;
+	}
 
 	public ArrayList<Location> aStar(Location a, Location b) {
-		Location loc = a;
 		if(open.size() == 0) {
-			open.add(loc);
+			open.add(new Daddy(a, a, 0));
 		}
 		Location location = a;
-		while(location != b) {
+		while(a != b) {
 			// http://theory.stanford.edu/~amitp/GameProgramming/ImplementationNotes.html
-
+			Daddy current = openLowest();
+			a = current.getLoc();
+			open.remove(current);
+			closed.add(current);
+			ArrayList<Location> locs = getAllAdjacent(location);
+			for(int i = 0; i < locs.size(); i++) {
+				Location loc = locs.get(i);
+				int cost = getPythag(location, b) + getCost(a);
+				int cc = current.getRank();
+				if(open.contains(loc) && cost < cc) {
+					open.remove(current);
+				}
+				if(closed.contains(loc) && cost < cc) {
+					closed.remove(current);
+				}
+				if(!(open.contains(loc) && closed.contains(loc))) {
+					open.add(new Daddy(loc, a, cost));
+					a = loc;
+				}
+			}
 		}
 		return null;
 		// Heuristic: f(n) = g(n) + h(n)
 		// Need g(n) and h(n) to be equally weighted
+		// g(n) = pythag
+		// h(n) individual weights
 	}
 
 	public Location getMoveLocation() {
